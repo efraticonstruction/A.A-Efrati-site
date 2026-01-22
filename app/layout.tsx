f0ac3a0edaacb26848149bb95033dbaebe561740
efraticonstruction <A@a-efrati.com>
2026-01-22 21:05:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: 'א.א אפרתי - שיפוצים וגמר',
-  description: 'עבודות גמר ושיפוצים ברמה הגבוהה ביותר',
-}
-
 export default function RootLayout({
   children,
 }: {
